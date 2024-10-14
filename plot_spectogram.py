bd6f2f778292960d62d67514460bb21f239cc7b2
Serhat <37984665+serhatseval@users.noreply.github.com>
2024-10-14 12:58:02 +0200

    Plotting Spectogram

--- plot_spectogram.py
+++ plot_spectogram.py
@@ -1,5 +1,30 @@
-def main(): 
-    print("Hello World!")   
+import argparse
+from matplotlib import pyplot as plt
+import soundfile as sf
+import pathlib as pl
+import librosa
+import numpy as np
+from datetime import datetime
+
+def plot_spectogram(signal, signal_rate, outputpath):
+    stft = librosa.stft(signal)
+    spectogram = np.abs(stft)
+    spectogram_db = librosa.amplitude_to_db(spectogram)
+    plt.figure(figsize=(14, 5))
+    img = librosa.display.specshow(spectogram_db, sr=signal_rate, x_axis= 'time', y_axis='log', cmap='inferno')
+    outputpath.mkdir(parents=True, exist_ok=True)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    plt.savefig(outputpath / f'spectogram_{timestamp}.png')
+    plt.show()
+
+
+def main(audiofile): 
+    signal, signal_rate = sf.read(audiofile)
+    print(f"Signal Rate: {signal_rate}")
+    plot_spectogram(signal, signal_rate, pl.Path('OutputFiles/'))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Plot Spectogram')
+    parser.add_argument('audiofile', type=str, help='Path to the audio file')
+    args = parser.parse_args()
+    main(args.audiofile)
