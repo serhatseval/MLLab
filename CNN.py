ee6a6c1c2788303d83603469f800e27e82c8d393
Serhat <37984665+serhatseval@users.noreply.github.com>
2024-10-21 14:54:21 +0200

    Push

---
+++ CNN.py
@@ -0,0 +1,123 @@
+import os
+import pandas as pd
+from torchvision.io import read_image, ImageReadMode
+import torch
+from torch import nn, Tensor
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+annotations_file = '.\..\..\\testData\labels.csv'
+img_dir = '.\..\..\\testData\images'
+img_size = 20
+img_height = 1600
+img_width = 1000
+features = 2
+batch_size = 2
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path, mode=ImageReadMode.GRAY)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+training_data = CustomImageDataset(annotations_file, img_dir, transform=transforms.ConvertImageDtype(torch.float32),
+                                   target_transform=None)
+test_data = CustomImageDataset(annotations_file, img_dir, transform=transforms.ConvertImageDtype(torch.float32),
+                               target_transform=None)
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(img_height*img_width, img_height*img_width),
+            nn.ReLU(),
+            nn.Linear(img_height*img_width, features)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
--- plot_spectogram.py
+++ plot_spectogram.py
@@ -10,16 +10,20 @@ def plot_spectogram(signal, signal_rate, outputpath):
     stft = librosa.stft(signal)
     spectogram = np.abs(stft)
     spectogram_db = librosa.amplitude_to_db(spectogram)
-    plt.figure(figsize=(14, 5))
-    img = librosa.display.specshow(spectogram_db, sr=signal_rate, x_axis= 'time', y_axis='log', cmap='Grays')
+    plt.figure(figsize=(16, 10), dpi=100, constrained_layout=True)
+    plt.axis('off')
+    img = librosa.display.specshow(spectogram_db, sr=signal_rate, x_axis=None, y_axis='log', cmap='Grays')
     outputpath.mkdir(parents=True, exist_ok=True)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     plt.savefig(outputpath / f'spectogram_{timestamp}.png')
     plt.show()
     return spectogram_db
 
-def main(audiofile): 
-    signal, signal_rate = sf.read(audiofile)
+file_path = "C:\\Users\Marcel\Desktop\IML\daps\daps\ipad_confroom1\\f2_script1_ipad_confroom1.wav"
+def main():
+    print(f"Original audio duration: {librosa.get_duration(path=file_path)} seconds")
+    signal, signal_rate = sf.read(file_path, frames=0)
+    signal, signal_rate = sf.read(file_path, start=5*signal_rate, stop=15*signal_rate)
     print(f"Signal Rate: {signal_rate}")
     spectrogram_db = plot_spectogram(signal, signal_rate, pl.Path('OutputFiles/'))
     print(f"Audio Duration: {len(signal)/signal_rate:.2f} seconds")
@@ -27,7 +31,7 @@ def main(audiofile):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Plot Spectogram')
-    parser.add_argument('audiofile', type=str, help='Path to the audio file')
-    args = parser.parse_args()
-    main(args.audiofile)
+    #parser = argparse.ArgumentParser(description='Plot Spectogram')
+    #parser.add_argument('audiofile', type=str, help='Path to the audio file')
+    #args = parser.parse_args()
+    main()
